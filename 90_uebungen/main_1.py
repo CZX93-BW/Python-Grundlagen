ae95c25ee6912c0e22f99e0575cf265172868c6d
CZX93-BW <yunimoon2012@gmail.com>
2026-09-03 06:57:30 +0200

    feat(asset): implement add note

---
+++ 90_uebungen/main_1.py
@@ -0,0 +1,28 @@
+notes = [
+    {"title": "Einkauf", "text": "Milch, Brot, Eier"},
+    {"title": "Arbeit", "text": "Backendcall um 11"},
+]
+
+
+def show_notes():
+    for note in notes:
+        print(f"Title: {note['title']}, Text: {note['text']}")
+
+
+def add_note():
+    new_note = {"title": "Schule", "text": "Hausaufgaben machen"}
+    notes.append(new_note)
+
+
+def delete_note():
+    pass
+
+
+def update_note():
+    pass
+
+
+add_note()
+# delete_note()
+# update_note()
+show_notes()
